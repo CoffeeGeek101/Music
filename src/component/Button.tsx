8808bb0bc69145d9338872aacddae677819a0712
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-17 10:01:17 +0530

    reusable button added

---
+++ src/component/Button.tsx
@@ -0,0 +1,42 @@
+import { cn } from '@/lib/util'
+import { cva, VariantProps } from 'class-variance-authority'
+import React, { ButtonHTMLAttributes } from 'react'
+
+const ButtonVariantProps = cva(
+    'active:scale-95 rounded-full hover:cursor-pointer shadow-lg shadow-[#507fff7e]',
+    {
+        variants : {
+            variant : {
+                main : 'bg-blue-500',
+                auth : 'bg-white text-black hover:bg-blue-200',
+                backy : 'bg-white'
+            },
+            size :{
+                free : 'px-20 py-3',
+                modal : 'w-full py-3 text-center'
+            }
+        },
+        defaultVariants :{
+            variant : 'main',
+            size : 'free'
+        }
+    }
+)
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof ButtonVariantProps>{
+    loading ?: boolean
+}
+
+const Button : React.FC<IButton> = ({className, variant, size, children, loading, ...props}) => {
+  return (
+    <button
+    className={cn(ButtonVariantProps({variant, size, className}))}
+    {...props}
+    disabled={loading}
+    >
+    {children}
+    </button>
+  )
+}
+
+export default Button
