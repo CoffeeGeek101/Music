a3f205be250329ff9571735ac12f34d50723ea62
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-18 11:49:44 +0530

    :safety_vest:added googleIds checker

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,3 +6,16 @@ export const BASE_URL = 'https://api.spotify.com/v1';
 export function cn(...inputs : ClassValue[]){
     return twMerge(clsx(inputs));
 }
+
+export function getGoogleIds(){
+    const client_id = process.env.GOOGLE_CLIENT_ID;
+    const client_secret = process.env.GOOGLE_CLIENT_SECRET;
+
+    if(!client_id || client_id.length === 0){
+        throw new Error('Missing Google client ID');
+    }
+    if(!client_secret || client_secret.length === 0){
+        throw new Error('Missing Google client secret');
+    }
+    return {client_id, client_secret}
+}
