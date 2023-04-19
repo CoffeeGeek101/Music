737fa4fcda399f0466a7544acd88f8328f8d7575
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-19 21:16:08 +0530

    :heavy_plus_sign: added languages for songs

---
+++ src/lib/language.ts
@@ -0,0 +1,43 @@
+export const lang = [
+    { 
+        code :'bn_IN',
+        name : 'Bengali'    
+    },
+    {
+        code :'en_IN',
+        name : 'English'   
+    },
+    {
+        code : 'gu_IN',
+        name:'Gujarati'
+    },
+    {
+        code : 'hi_IN',
+        name:'Hindi'
+    },
+    {
+        code : 'kn_IN',
+        name:'Kannada'
+    },
+    {
+        code : 'ml_IN',
+        name:'Malayalam'
+    },
+    {
+        code : 'mr_IN',
+        name:'Marathi'
+    },
+    {
+        code : 'pa_IN',
+        name:'Punjabi'
+    },
+    {
+        code : 'ta_IN',
+        name:'Tamil'
+    },
+    {
+        code : 'te_IN',
+        name:'Telugu'
+    },
+
+];
