f1e270049efa3f1e8e2ca92be86cdfb9fb513e6d
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-22 16:36:55 +0530

    :page_facing_up: added custom genres

---
+++ src/lib/genre.ts
@@ -0,0 +1,18 @@
+export const genres = [
+    'Pop',
+    'Hip-hop',
+    'RnB',
+    'Rock',
+    'Indie',
+    'Electronic',
+    'Country',
+    'Dance',
+    'K-Pop',
+    'Jazz',
+    'Classical',
+    'Metal',
+    'Blues',
+    'Funk',
+    'Punk',
+    'Folk'
+  ];
