cd4ffc90e1500fcb6307ea31d2d5eca17a924ea1
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-28 09:38:02 +0530

    :ambulance: making the fallback comp reusable

--- src/component/FallBack.tsx
+++ src/component/FallBack.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
-const FallBack = () => {
+interface IFallBack{
+  label_A : string;
+  label_B : string;
+}
+
+const FallBack : React.FC<IFallBack> = ({label_A,label_B}) => {
   return (
-    <div className='flex flex-col items-center'>
-        <p 
-        style={{textShadow:'1px 1px 20px rgba(0, 110, 255, 0.805)'}}
-        className='text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-white to-[#899fff]'>Seems like, you are not Logged in.</p>
-        <p className='text-lg text-gray-500'>Login to continue</p>
+    <div className='flex flex-row justify-center items-center h-[70vh]'>
+      <div className='flex flex-col items-center'>
+          <p 
+          style={{textShadow:'1px 1px 20px rgba(0, 110, 255, 0.805)'}}
+          className='text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-white to-[#899fff]'>{label_A}</p>
+          <p className='text-lg text-gray-500'>{label_B}</p>
+      </div>
     </div>
   )
 }
