819fbbfa8b94fe27b7c4c3905fa2d5b361da50d2
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-28 09:43:10 +0530

    :seedling: setting up Likedsongs API

---
+++ src/actions/getLikedSongs.ts
@@ -0,0 +1,42 @@
+import getValid_token from "@/lib/SpotityClient"
+import { BASE_URL } from "@/lib/util";
+import axios from "axios";
+import { getCurrentUser } from "./getCurrentUser";
+
+export const getLikedSongs = async() => {
+    
+    const token = await getValid_token();
+    const user = await getCurrentUser();
+
+    if(!user){
+        throw new Error('NO user, please login')
+    }
+
+    const likedsong = user.likedSongs;
+
+    const batch = 50;
+
+    const tracks = [];
+
+    for(let i=0; i < likedsong.length; i+=batch){
+
+        const current_batch = likedsong.slice(i, i+batch);
+        const working_batch = current_batch.join(',');
+
+        try{
+            const res = await axios.get(`${BASE_URL}/tracks?ids=${working_batch}`,
+                {
+                    headers : {
+                        "Authorization" : `Bearer ${token}`
+                    }
+                }
+            )
+            const batch_data = res.data?.tracks;
+            tracks.push(...batch_data);
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    return tracks;
+}
