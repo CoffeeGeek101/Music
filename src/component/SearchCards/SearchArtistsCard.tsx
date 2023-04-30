0b8ba241bf831aad5d92346c4ca06f6efe878e41
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-30 14:19:50 +0530

    :lock: tracking listening pattern

--- src/component/SearchCards/SearchArtistsCard.tsx
+++ src/component/SearchCards/SearchArtistsCard.tsx
@@ -1,13 +1,19 @@
+"use client"
+
 import React from 'react'
 
 interface ISearchArtist{
-    artist : {}
+    artist : {};
+    onClick : ()=>void;
 }
 
-const SearchArtistsCard : React.FC<ISearchArtist> = ({artist} : any) => {
+const SearchArtistsCard : React.FC<ISearchArtist> = ({artist, onClick} : any) => {
+  console.log(artist)
   return (
-    <div className='flex flex-row gap-2 items-center'>
-    <img src={artist.images[0].url} className='h-[80px] w-[80px] md:h-[90px] md:w-[90px] object-cover rounded-full'/>
+    <div 
+    onClick={onClick}
+    className='flex flex-row gap-2 items-center'>
+    <img src={artist?.images[0]?.url} className='h-[80px] w-[80px] md:h-[90px] md:w-[90px] object-cover rounded-full'/>
     <div className='flex flex-col items-start'>
         <p className='text-[10px] w-[130px] md:w-[320px] lg:w-[400px] truncate'>{artist.name}</p>
         <p className='text-[8px] text-gray-500'>Artist</p>
