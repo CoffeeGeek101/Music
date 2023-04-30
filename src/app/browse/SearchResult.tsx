3ad717a30b00d5edae65ef294cccbadedc8dbc3e
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-30 14:18:06 +0530

    :sparkles: implementing listening pattern tracker

--- src/app/browse/SearchResult.tsx
+++ src/app/browse/SearchResult.tsx
@@ -1,6 +1,8 @@
 "use client"
 import SearchArtistsCard from '@/component/SearchCards/SearchArtistsCard';
 import SearchSongCard from '@/component/SearchCards/SearchSongCard';
+import useArtistHistory from '@/hooks/ArtistHistoryHook';
+import useSongHistory from '@/hooks/SongHistoryHook';
 import React from 'react'
 
 interface ISearchresult{
@@ -9,10 +11,20 @@ interface ISearchresult{
 }
 
 const SearchResult : React.FC<ISearchresult> = ({searchRes, search} : any) => {
+   
     const {artists} = searchRes;
     const {tracks} = searchRes;
 
-    console.log(artists)
+
+    const HandleSongHistory = ( e : string) =>{
+        const {recordHistory} = useSongHistory({songhistoryId : e})
+        recordHistory();
+        }
+
+    const HandleArtistHistory = ( e : string) =>{
+        const {recordHistory} = useArtistHistory({artistId : e})
+        recordHistory();
+        }
 
   return (
     <div className='p-5 h-[610px] bg-slate-950/20 bg-gradient-to-r from-slate-900 to-blue-900/50 mt-5 rounded-tl-lg overflow-y-scroll overflow-x-hidden flex flex-col gap-5'>
@@ -23,7 +35,7 @@ const SearchResult : React.FC<ISearchresult> = ({searchRes, search} : any) => {
         <hr/>
         {
             tracks.items.slice(0,5).map((track : any)=>(
-                <SearchSongCard key={track.id} track={track}/>
+                <SearchSongCard onClick={()=>HandleSongHistory(track.id)} key={track.id} track={track}/>
             ))
         }
        </div>
@@ -35,7 +47,7 @@ const SearchResult : React.FC<ISearchresult> = ({searchRes, search} : any) => {
         <hr/>
         {
             artists.items.slice(0,4).map((artist : any)=>(
-                <SearchArtistsCard key={artist.id} artist={artist}/>
+                <SearchArtistsCard onClick={()=>HandleArtistHistory(artist.id)}  key={artist.id} artist={artist}/>
             ))
         }
        </div>
