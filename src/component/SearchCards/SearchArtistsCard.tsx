45e0b4b6521455a6bec0433a4ae611b2d018199b
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-05-01 22:51:07 +0530

    :lipstick: updated UI

--- src/component/SearchCards/SearchArtistsCard.tsx
+++ src/component/SearchCards/SearchArtistsCard.tsx
@@ -12,10 +12,10 @@ const SearchArtistsCard : React.FC<ISearchArtist> = ({artist, onClick} : any) =>
   return (
     <div 
     onClick={onClick}
-    className='flex flex-row gap-2 items-center'>
+    className='flex flex-row gap-2 items-center hover:cursor-pointer rounded-l-full hover:bg-gradient-to-l from-slate-100/0 to-blue-600/50'>
     <img src={artist?.images[0]?.url} className='h-[80px] w-[80px] md:h-[90px] md:w-[90px] object-cover rounded-full'/>
     <div className='flex flex-col items-start'>
-        <p className='text-[10px] w-[130px] md:w-[320px] lg:w-[400px] truncate'>{artist.name}</p>
+        <p className='text-[10px] md:text-base lg:text-base w-[130px] md:w-[320px] lg:w-[400px] truncate'>{artist.name}</p>
         <p className='text-[8px] text-gray-500'>Artist</p>
     </div>
     </div>
--- src/component/SearchCards/SearchSongCard.tsx
+++ src/component/SearchCards/SearchSongCard.tsx
@@ -11,10 +11,10 @@ const SearchSongCard : React.FC<ISearchSongs> = ({track, onClick} : any) => {
   return (
     <div 
     onClick={onClick}
-    className='flex flex-row gap-2 items-center'>
+    className='flex flex-row gap-2 items-center hover:cursor-pointer hover:bg-gradient-to-l from-slate-100/0 to-blue-600/50'>
         <img src={track.album.images[0].url} className='h-[70px] w-[70px] md:h-[90px] md:w-[90px]'/>
         <div className='flex flex-col items-start'>
-            <p className='text-[10px] text-base w-[130px] md:w-[320px] lg:w-[400px] truncate'>{track.name}</p>
+            <p className='text-[11px] md:text-base lg:text-base w-[130px] md:w-[320px] lg:w-[400px] truncate'>{track.name}</p>
             <div className='flex flex-row gap-1 items-center'>
                 {track.explicit && <p className='bg-slate-600 px-[2px] rounded-sm text-[8px]'>E</p>}
                 <p className='text-[9px] md:text-[12px] text-slate-500'>{track.artists[0].name}</p>
