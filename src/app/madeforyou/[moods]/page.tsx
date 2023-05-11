0bdf8fe0500190a49ad21d886950df484b21c9ea
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-05-11 11:19:17 +0530

    added fallback for no user and padding for playerSDK

--- src/app/madeforyou/[moods]/page.tsx
+++ src/app/madeforyou/[moods]/page.tsx
@@ -1,5 +1,6 @@
 import { getCurrentUser } from '@/actions/getCurrentUser';
 import getRecomendation from '@/actions/getReccomendation';
+import FallBack from '@/component/FallBack';
 import { mood } from '@/lib/moods';
 import React from 'react'
 import MoodClient from './moodClient';
@@ -14,6 +15,16 @@ const page = async ({params}:{params : IMoods}) => {
     const user = await getCurrentUser();
     const rec_song = await getRecomendation({moods})
     const mood_expressions = mood.find((m)=>m.name === moods)
+
+    if(!user){
+      return (
+          <FallBack 
+          label_A='Seems like, you are not Logged in.' 
+          label_B='Login to continue'
+          />
+      )
+    }
+
   return (
     <div>
         <MoodClient moody_songs={rec_song} expression={mood_expressions} user={user}/>
