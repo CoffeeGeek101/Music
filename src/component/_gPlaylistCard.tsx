c47d199e02f4887f6ea7f330884b245172e22f2f
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-05-11 11:20:11 +0530

    :boom:fixed the ui layout

--- src/component/_gPlaylistCard.tsx
+++ src/component/_gPlaylistCard.tsx
@@ -12,14 +12,16 @@ const GPlaylistCard : React.FC<IPlaylistCard> = ({
     des,
 }) => {
   return (
-    <div className='flex flex-col justify-start w-[300px] md:w-[200px] lg:w-[180px] gap-3'>
-        <img
-        src={imgSrc}
-        alt='playlists'
-        className='rounded-md shadow-lg shadow-[#4171ff55]'
-        />
-        <p className='text-lg font-bold bg-clip-text text-transparent bg-gradient-to-br from-white to-[#899fff]'>{name}</p>
-        {<p className='text-xs font-light text-slate-400 mt-[-5px]'>{des}</p>}
+    <div className='flex flex-col justify-start w-[300px] md:w-[200px] lg:w-[180px] lg:h-[180px] gap-2'>
+        <div className='h-full w-full'>
+          <img
+          src={imgSrc}
+          alt='playlists'
+          className='rounded-md shadow-lg shadow-[#4171ff55] w-full h-full object-cover'
+          />
+          <p className='text-lg font-bold bg-clip-text text-transparent bg-gradient-to-br from-white to-[#899fff]'>{name}</p>
+          {<p className='text-xs font-light text-slate-400 mt-[-5px]'>{des}</p>}
+        </div>
     </div>
   )
 }
